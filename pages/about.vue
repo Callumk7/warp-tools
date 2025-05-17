b183600161859c40385eb1f0ed6d6a6fa6134a4f
Callum Kloos <Callumkloos@gmail.com>
2025-05-17 15:39:54 +0100

    init

---
+++ pages/about.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>about</h1>
+    <UButton>Click Me</UButton>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const {data} = await useFetch('/api/dummy');
+</script>
+
+<template>
+    <Navigation />
+    <Table />
+</template>
