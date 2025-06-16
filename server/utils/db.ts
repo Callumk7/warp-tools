eeade6edb3acd5eac6d44babd8f810dfd8e9c44c
Callum Kloos <Callumkloos@gmail.com>
2025-06-16 15:01:31 +0100

    linting

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,12 +1,12 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
-import * as schema from '../../db/schema';
-import * as authSchema from '../../db/auth-schema';
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as schema from "../../db/schema";
+import * as authSchema from "../../db/auth-schema";
 
 // Create a PostgreSQL connection pool
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+	connectionString: process.env.DATABASE_URL,
 });
 
 // Create a DrizzleORM instance
-export const db = drizzle(pool, {schema: { ...schema,...authSchema } });
+export const db = drizzle(pool, { schema: { ...schema, ...authSchema } });
